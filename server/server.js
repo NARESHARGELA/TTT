2b1ddbe117f5f459649c3faa50b6197cf572bf20
Naresh <nareshargela200@gmail.com>
2024-01-27 15:44:16 +0530

    netlify

--- server/server.js
+++ server/server.js
@@ -17,14 +17,7 @@ app.use(cors());
 app.use("/api/portfolio", portfolioRoutes);
 
 const Port = process.env.PORT || 5000;
-// const path = require('path')
 
-// if(process.env.NODE_ENV === 'production'){
-//     app.use(express.static(path.join(__dirname,'client/build')))
-//     app.get('*',(req,res)=>{
-//         res.sendFile(path.join(__dirname,"client/build/index.html"))
-//     })
-// }
 
 app.listen(Port, () => {
   console.log("Server is Started");
